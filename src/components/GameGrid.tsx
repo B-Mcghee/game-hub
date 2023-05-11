32fa2b2e7c985a566f4163a13edb2b88f2fb940a
Brandon McGhee <bmcghee.dev@gmail.com>
2023-05-11 16:57:01 -0400

    Added custom hook to fetch games

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from "react";
-import apiClient from "../services/api-client";
 import { Text } from "@chakra-ui/react";
+import useGames from "../hooks/useGames";
 
 const GameGrid = () => {
+  const { games, error } = useGames();
   return (
     <>
       {error && <Text>{error}</Text>}
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
+import { CanceledError } from "axios";
 
 interface Game {
     id: number;
@@ -13,16 +14,19 @@ interface Game {
 const useGames = () => {
     const [games, setGames] = useState<Game[]>([]);
     const [error, setError] = useState([]);
-    const [isLoading, setLoading] = useState([])
+    const [isLoading, setLoading] = useState(false)
     useEffect(() => {
+        const controller = new AbortController();
       apiClient
-        .get<FetchGamesResponse>("/games")
+        .get<FetchGamesResponse>("/games", {signal: controller.signal})
         .then((res) => setGames(res.data.results))
         .catch((err) => 
         {
-            if(err)
+            if(err instanceof CanceledError) return;
             setError(err.message)
         });
+
+        return () => controller.abort();
     }, []);  
     return {games, error, setGames, isLoading}
 }
