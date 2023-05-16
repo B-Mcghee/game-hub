d5da2ad270d46c7f45d76a6245f9c4d32ba6b5bb
Brandon McGhee <bmcghee.dev@gmail.com>
2023-05-16 07:28:37 -0400

    Added filtering games by platform

--- src/hooks/useData.ts
+++ src/hooks/useData.ts
@@ -1,14 +1,6 @@
 import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
 import { AxiosRequestConfig, CanceledError } from "axios";
-
-
-export interface Platform {
-    id: number;
-    name: string;
-    slug: string;
-  }
-
   
   export interface FetchResponse<T> {
     count: number;
