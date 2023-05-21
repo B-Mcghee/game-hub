cc7b181721b5a6417cc9c73622074acd35cdabab
Brandon McGhee <bmcghee.dev@gmail.com>
2023-05-21 12:53:42 -0400

    Commented out useData to keep for future reference

--- src/hooks/useData.ts
+++ src/hooks/useData.ts
@@ -1,34 +1,34 @@
-import { useEffect, useState } from "react";
-import apiClient, { FetchResponse } from "../services/api-client";
-import { AxiosRequestConfig, CanceledError } from "axios";
+// import { useEffect, useState } from "react";
+// import apiClient, { FetchResponse } from "../services/api-client";
+// import { AxiosRequestConfig, CanceledError } from "axios";
   
 
-const useData = <T>(endpoint:string, requestConfig?: AxiosRequestConfig, deps?: any[]) => {
-    const [data, setData] = useState<T[]>([]);
-    const [error, setError] = useState();
-    const [isLoading, setLoading] = useState(false);
+// const useData = <T>(endpoint:string, requestConfig?: AxiosRequestConfig, deps?: any[]) => {
+//     const [data, setData] = useState<T[]>([]);
+//     const [error, setError] = useState();
+//     const [isLoading, setLoading] = useState(false);
 
-    useEffect(() => {
-        const controller = new AbortController();
+//     useEffect(() => {
+//         const controller = new AbortController();
 
-        setLoading(true)
-      apiClient
-        .get<FetchResponse<T>>(endpoint, {signal: controller.signal, ...requestConfig})
-        .then((res) => {
-          setData(res.data.results)
-          setLoading(false)
-        })
-        .catch((err) => 
-        {
-            if(err instanceof CanceledError) return;
-            setError(err.message)
-            setLoading(false)
-        })
-        // .finally(() => setLoading(true));
+//         setLoading(true)
+//       apiClient
+//         .get<FetchResponse<T>>(endpoint, {signal: controller.signal, ...requestConfig})
+//         .then((res) => {
+//           setData(res.data.results)
+//           setLoading(false)
+//         })
+//         .catch((err) => 
+//         {
+//             if(err instanceof CanceledError) return;
+//             setError(err.message)
+//             setLoading(false)
+//         })
+//         // .finally(() => setLoading(true));
 
-        return () => controller.abort();
-    }, deps ? [...deps]: []);  
-    return {data, error, setData, isLoading}
-}
+//         return () => controller.abort();
+//     }, deps ? [...deps]: []);  
+//     return {data, error, setData, isLoading}
+// }
 
-export default useData
+// export default useData
