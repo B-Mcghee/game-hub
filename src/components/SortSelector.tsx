65a21f26b37c92e5fa3b088e6ebe420a6aee4e8b
Brandon McGhee <bmcghee.dev@gmail.com>
2023-05-23 08:09:31 -0400

    Refactored - global state management with Zustand

--- src/components/SortSelector.tsx
+++ src/components/SortSelector.tsx
@@ -1,12 +1,11 @@
 import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
 import { BsChevronDown } from "react-icons/bs";
+import useGameQueryStore from "../store";
 
-interface Props {
-  onSelectSortOrder: (sortOrder: string) => void;
-  sortOrder: string;
-}
+const SortSelector = () => {
+  const setSortOrder = useGameQueryStore((s) => s.setSortOrder);
+  const sortOrder = useGameQueryStore((s) => s.gameQuery.sortOrder);
 
-const SortSelector = ({ onSelectSortOrder, sortOrder }: Props) => {
   const sortOrders = [
     { value: "", label: "Relevance" },
     { value: "-added", label: "Date Added" },
@@ -18,6 +17,7 @@ const SortSelector = ({ onSelectSortOrder, sortOrder }: Props) => {
   const currentSortOrder = sortOrders.find(
     (order) => order.value === sortOrder
   );
+
   return (
     <Menu>
       <MenuButton as={Button} rightIcon={<BsChevronDown />}>
@@ -26,7 +26,7 @@ const SortSelector = ({ onSelectSortOrder, sortOrder }: Props) => {
       <MenuList>
         {sortOrders.map((order) => (
           <MenuItem
-            onClick={() => onSelectSortOrder(order.value)}
+            onClick={() => setSortOrder(order.value)}
             key={order.value}
             value={order.value}
           >
---
+++ src/store.ts
@@ -0,0 +1,33 @@
+import { create } from "zustand";
+
+
+
+interface GameQuery {
+    genreId?: number;
+    platformId?: number;
+    sortOrder?: string;
+    searchText?: string;
+  }
+
+interface GameQueryStore {
+    gameQuery: GameQuery;
+    setGenreId: (genreId:number) => void;
+    setPlatformId: (platformId:number) => void;
+    setSearchText: (text: string) => void;
+    setSortOrder: (sortOrder:string) => void;
+}
+
+const useGameQueryStore = create<GameQueryStore>(set => ({
+    gameQuery:{},
+    setGenreId: (genreId:number) => 
+        set((store) => ({gameQuery: {...store.gameQuery, genreId}})),
+    setPlatformId: (platformId:number) => 
+        set((store) => ({gameQuery: {...store.gameQuery, platformId}})),
+    setSearchText: (searchText) => 
+        set(() => ({gameQuery: {searchText}})),
+    setSortOrder: (sortOrder) => 
+        set(store => ({gameQuery: {...store.gameQuery, sortOrder}}))
+
+}));
+
+export default useGameQueryStore;
